f3225934e84f02cbda4e488e227f94010839e3f5
cw-roy <curtis_roy@msn.com>
2023-05-13 11:55:28 -0700

    Update search page JS

---
+++ outdoors/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Homepage from './components/Homepage';
+import NationalParksSearchPage from './components/NationalParksSearchPage';
+import MountainsInformationPage from './components/MountainsInformationPage';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+      <NationalParksSearchPage />
+      <MountainsInformationPage />
+    </div>
+  );
+}
+
+export default App;
