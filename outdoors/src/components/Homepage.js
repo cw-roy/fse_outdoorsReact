5eccc1daf548181c2d6774eb01efeb27c5ca6cca
cw-roy <curtis_roy@msn.com>
2023-05-17 14:26:19 -0700

    Update Homepage items

--- outdoors/src/components/Homepage.js
+++ outdoors/src/components/Homepage.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './Homepage.css'; // Import the CSS file for styling
 
 function Homepage() {
   return (
-    <div>
-      <h1>Our Organization</h1>
-      <p>Welcome to our website! Explore the great outdoors with us.</p>
-      <a href="/national-parks">National Parks</a>
-      <a href="/mountains">Mountains</a>
+    <div className="homepage-container">
+      <h1 className="homepage-title">Enjoy the Outdoors!</h1>
+      <p className="homepage-description">Welcome! Explore the great outdoors with us.</p>
+      <div className="homepage-links">
+        <a href="/national-parks" className="homepage-link">National Parks</a>
+        <a href="/mountains" className="homepage-link">Mountains</a>
+      </div>
     </div>
   );
 }
 
 export default Homepage;
+
