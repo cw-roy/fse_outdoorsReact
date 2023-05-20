78d0583a66390605b37a3583f5dda42140b0a35d
cw-roy <curtis_roy@msn.com>
2023-05-20 10:33:30 -0700

    Build app.js

--- outdoors/src/App.js
+++ outdoors/src/App.js
@@ -1,14 +1,96 @@
-import React from 'react';
-import Homepage from './components/Homepage';
-import NationalParksSearchPage from './components/NationalParksSearchPage';
-import MountainsInformationPage from './components/MountainsInformationPage';
+import React, { useState } from 'react';
+import './App.css';
+import locationsData from '../data/locations.json';
+import parkTypesData from '../data/parktypes.json';
+// import nationalParksData from '../data/nationalparks.json'
 
 function App() {
+  const [searchBy, setSearchBy] = useState('location');
+  const [selectedLocation, setSelectedLocation] = useState('');
+  const [selectedParkType, setselectedParkType] = useState('');
+  const [nationalParks, setNationalParks] = useState([]);
+
+  useEffect(() => {
+    // Fetch national parks data from the file
+    const fetchNationalParksData = async () => {
+      try {
+        const response = await fetch('../data/nationalparks.json');
+        const data = await response.json();
+        setNationalParks(data.parks);
+      } catch (error) {
+        console.error('Error fetching national parks data:', error);
+      }
+    };
+
+    fetchNationalParksData();
+  }, []);
+
+  const handleSearchByChange = (e) => {
+    setSearchBy(e.target.value);
+  };
+
+  const handleLocationChange = (e) => {
+    setSelectedLocation(e.target.value);
+  };
+
+  const handleParkTypeChange = (e) => {
+    setSelectedParkType(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle the search submission, you can add your logic here
+    console.log('Submitted!');
+    console.log('Search By:', searchBy);
+    console.log('Selected Location:', selectedLocation);
+    console.log('Selected Park Type:', selectedParkType);
+  };
+
   return (
-    <div>
-      <Homepage />
-      <NationalParksSearchPage />
-      <MountainsInformationPage />
+    <div className="App">
+      <h1>National Parks Search</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Search By:
+            <select value={searchBy} onChange={handleSearchByChange}>
+              <option value="location">Location</option>
+              <option value="parkType">Park Type</option>
+            </select>
+          </label>
+        </div>
+        {searchBy === 'location' && (
+          <div>
+            <label>
+              Location:
+              <select value={selectedLocation} onChange={handleLocationChange}>
+                <option value="">Select a location</option>
+                {locationsData.map((location) => (
+                  <option key={location} value={location}>
+                    {location}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+        )}
+        {searchBy === 'parkType' && (
+          <div>
+            <label>
+              Park Type:
+              <select value={selectedParkType} onChange={handleParkTypeChange}>
+                <option value="">Select a park type</option>
+                {parkTypesData.map((parkType) => (
+                  <option key={parkType} value={parkType}>
+                    {parkType}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+        )}
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 }
